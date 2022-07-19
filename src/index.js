8c7b7a5511df4e9a5448d46a24339216b6e6421e
Bruno Roja <brunojlee@hotmail.com>
2022-07-19 19:43:55 -0400

    style: :lipstick: Add main color to page body

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
-reportWebVitals();
