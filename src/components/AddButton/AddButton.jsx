be907e87a9a7523759d964f0a19928ab2bc1c78d
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 17:28:08 -0400

    feat: :sparkles: Add Token Button Init

---
+++ src/components/AddButton/AddButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AddButton.css';
+
+export default function AddButton() {
+  return (
+    <div className="addButtonContainer">
+      <button
+        className="addButton"
+        type="button"
+      >
+        Add Token
+      </button>
+
+    </div>
+  );
+}
