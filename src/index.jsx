0e6e7312a7e6b8c06f9a6b3bd086b0ee32fa63d1
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 17:29:03 -0400

    refactor: :recycle: Index Lint Fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <App />
-  </HashRouter>
+  </HashRouter>,
 );
-
