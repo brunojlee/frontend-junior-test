4410d3c555fbd1f4a8fcb2bf0a6ca65a225cf8b2
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 01:39:17 -0400

    style: :art: Eslint implementation + Header changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="headerContainer">
+      <Logo />
+      <div className="" />
+    </div>
+  );
+}
