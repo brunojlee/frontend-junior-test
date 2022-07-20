51c00c5840035d86d07ca22e62684b46be22365e
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 18:45:58 -0400

    feat: :sparkles: AddToken Page Route add

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
+import AddToken from './pages/AddToken/AddToken';
 import Home from './pages/Home/Home';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/AddToken" element={<AddToken />} />
     </Switch>
   );
 }
