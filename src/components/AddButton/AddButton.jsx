46a8a92c7f31128477bd389c48d32d46be206317
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 18:44:57 -0400

    feat: :sparkles: AddButton Navigation init

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import WalletContext from '../../context/WalletContext';
+
 import './AddButton.css';
 
 export default function AddButton() {
+  const navigate = useNavigate();
+  const {
+    setLoading,
+    isButtonShow,
+    setIsButtonShow,
+  } = useContext(WalletContext);
+
+  const handleClick = () => {
+    setLoading(true);
+    navigate('/AddToken');
+    setIsButtonShow({ display: 'none' });
+  };
+
   return (
     <div className="addButtonContainer">
       <button
         className="addButton"
         type="button"
+        onClick={handleClick}
+        style={isButtonShow}
       >
         Add Token
       </button>
