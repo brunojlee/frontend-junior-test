747ff7b3a014eb48f3aabcece614fcd50c174d2e
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 03:27:44 -0400

    feat: :sparkles: Add star and tittle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { ReactComponent as Star } from '../../assets/shooting-star.svg';
+
 import './Header.css';
 
 export default function Header() {
   return (
     <div className="headerContainer">
-      <Logo />
-      <div className="" />
+      <Logo className="logo" />
+      <div className="lowHeaderContainer">
+        <div className="starContainer">
+          <Star className="star" />
+          <div className="title">Wish Wallet</div>
+        </div>
+      </div>
     </div>
   );
 }
