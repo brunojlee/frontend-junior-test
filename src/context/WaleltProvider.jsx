70e3287eb2449286f544dd1295e7ede4d11d9012
Bruno Roja <brunojlee@hotmail.com>
2022-07-20 18:31:46 -0400

    feat: :sparkles: Context Provider implementation

---
+++ src/context/WaleltProvider.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+import React, { useMemo, useState } from 'react';
+import WalletContext from './WalletContext';
+
+function WalletProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [isButtonShow, setIsButtonShow] = useState({ display: 'flex' });
+
+  const context = useMemo(() => ({
+    loading,
+    setLoading,
+    isButtonShow,
+    setIsButtonShow,
+  }), []);
+
+  return (
+
+    <WalletContext.Provider value={context}>
+      {children}
+    </WalletContext.Provider>
+  );
+}
+
+WalletProvider.propTypes = {
+  children: PropTypes.any,
+}.isRequired;
+
+export default WalletProvider;
